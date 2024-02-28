89690a4c525b40b38b47312211583b436020f672
Nikolas Louret <nikoul.ret@gmail.com>
2024-02-27 23:32:53 -0300

    [FEAT] adicao da query graphql

--- scripts/getRepositoriesData.py
+++ scripts/getRepositoriesData.py
@@ -10,22 +10,53 @@ api_url = 'https://api.github.com/graphql'
 
 headers = {'Authorization': 'Bearer %s' % API_KEY}
 
-def run_query(query): 
-    request = requests.post(url=api_url, json={'query': query}, headers=headers)
+
+def run_query(query):
+    request = requests.post(
+        url=api_url, json={'query': query}, headers=headers)
     if request.status_code == 200:
         return request.json()
     else:
-        raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
+        raise Exception("Query failed to run by returning code of {}. {}".format(
+            request.status_code, query))
+
 
 query = """
-
+{
+    search(query: "stars:>1", type: REPOSITORY, first: 50) {
+    edges {
+      node {
+        ... on Repository {
+          name
+          createdAt
+          updatedAt
+          primaryLanguage {
+            name
+          }
+          totalIssues: issues {
+            totalCount
+          }
+          closedIssues: issues(states: CLOSED) {
+            totalCount
+          }
+          pullRequests(states: MERGED) {
+            totalCount
+          }
+          releases {
+            totalCount
+          }
+        }
+      }
+    }
+  }
+}
 """
 
-result = run_query(query) 
+result = run_query(query)
 
 # Create the directory if not exists
 os.makedirs('dataset/json', exist_ok=True)
 
-# Save the result in a json archive 
-with open('scripts/dataset/json/data.json', 'w', encoding='utf-8') as f:
+# Save the result in a json archive
+with open('dataset/json/data.json', 'w', encoding='utf-8') as f:
     json.dump(result, f, ensure_ascii=False, indent=2)
